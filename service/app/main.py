5df05dbc74e474314fe9eee0734fc54e90d3b5fa
Link <link@dxmc.org>
2025-03-10 18:12:09 +0700

    new code 1003

--- service/app/main.py
+++ service/app/main.py
@@ -5,7 +5,7 @@ import requests
 import random
 from opentelemetry import trace
 from opentelemetry.trace.status import Status, StatusCode
-from opentelemetry._logs import set_logger_provider
+from opentelemetry._logs import set_logger_provider, set_logger_provider
 from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
     OTLPLogExporter,
 )
@@ -20,24 +20,68 @@ from opentelemetry.sdk.trace.export import (
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
-
-# random.seed(54321)
-trace.set_tracer_provider(TracerProvider())
-otlp_exporter = OTLPSpanExporter(insecure=True)
+tracer_provider = TracerProvider(
+    resource=Resource.create(
+        {
+            "service.name": "app_demo",
+            # "service.instance.id": "instance-12",
+        }
+    ),
+)
+# url = 'http://openobserve:5080/api/default/v1/traces'
+# headers = {"Authorization": "Basic YWRtaW5AYWRtaW4uY29tOmZ4VHl0MGFzbDBGbHVMRGk="}
+# otlp_exporter = OTLPSpanExporter(endpoint="localhost:4317")
+otlp_exporter = OTLPSpanExporter(endpoint="http://collector:4318", insecure=True)
 # trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
-trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+# trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
+tracer_provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+trace.set_tracer_provider(tracer_provider)
 
+########## LOGGING ##########
+# Create a logger provider
+logger_provider = LoggerProvider(
+    resource=Resource.create(
+        {
+            "service.name": "app_demo",
+            # "service.instance.id": "instance-12",
+        }
+    ),
+)
+
+# Cấu hình logs provider cho logs
+# exporter = OTLPLogExporter(endpoint="collector:4317", insecure=True)
+# logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+logger_provider.add_log_record_processor(
+    BatchLogRecordProcessor(OTLPLogExporter(insecure=True))
+)
+set_logger_provider(logger_provider)
+# logger = set_logger_provider(logger_provider)
+
+handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+print("handler", handler)
+
+# # Set up logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+# logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
 # Instrument FastAPI with OpenTelemetry
 FastAPIInstrumentor.instrument_app(app)
 
+# Endpoint đơn giản
+tracer = trace.get_tracer(__name__)
+with tracer.start_as_current_span("parent"):
+    print("trace", tracer)
+    print("Hello, World!")
+
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    # span = trace.get_tracer(__name__).start_span("root_span")
+    span = trace.get_current_span()
+    print("span", span)
+    span.end()
+    return {"message": "Hello 1111"}
 
 
 @app.get("/ping")
@@ -87,42 +131,45 @@ def external_api():
     return "ok"
 
 
-# @app.get("/log")
-# def log():
-#     logger_provider = LoggerProvider(
-#         resource=Resource.create(
-#             {
-#                 "service.name": "fastapiApp",
-#                 "service.instance.id": "instance-12",
-#             }
-#         ),
-#     )
-#     set_logger_provider(logger_provider)
 
-#     exporter = OTLPLogExporter(insecure=True)
-#     logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
-#     handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+@app.get("/log")
+def log():
+    # logger_provider = LoggerProvider(
+    #     resource=Resource.create(
+    #         {
+    #             "service.name": "demo_app",
+    #             # "service.instance.id": "instance-12",
+    #         }
+    #     ),
+    # )
+    # set_logger_provider(logger_provider)
 
-#     # Attach OTLP handler to root logger
-#     logging.getLogger().addHandler(handler)
+    # exporter = OTLPLogExporter(endpoint="collector:4317", insecure=True)
+    # logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+    # handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
 
-#     # Create different namespaced loggers
-#     # It is recommended to not use the root logger with OTLP handler
-#     # so telemetry is collected only for the application
-#     logger1 = logging.getLogger("myapp.area1")
-#     logger2 = logging.getLogger("myapp.area2")
+    # Attach OTLP handler to root logger
+    logging.getLogger().addHandler(handler)
 
-#     logger1.debug("Quick zephyrs blow, vexing daft Jim.")
-#     logger1.info("How quickly daft jumping zebras vex.")
-#     logger2.warning("Jail zesty vixen who grabbed pay from quack.")
-#     logger2.error("The five boxing wizards jump quickly.")
+    # Create different namespaced loggers
+    # It is recommended to not use the root logger with OTLP handler
+    # so telemetry is collected only for the application
+    logger1 = logging.getLogger("app_demo.log1")
+    logger2 = logging.getLogger("app_demo.log2")
+
+    logger1.debug("Quick zephyrs blow, vexing daft Jim.")
+    logger1.info("How quickly daft jumping zebras vex.")
+
+    logger2.warning("Jail zesty vixen who grabbed pay from quack.")
+    logger2.error("The five boxing wizards jump quickly.")
 
 
-#     # Trace context correlation
-#     tracer = trace.get_tracer(__name__)
-#     with tracer.start_as_current_span("foo"):
-#         # Do something
-#         logger2.error("Hyderabad, we have a major problem.")
+    # Trace context correlation
+    tracer = trace.get_tracer(__name__)
+    print("tracer", tracer)
+    # with tracer.start_as_current_span("foo"):
+    #     # Do something
+    #     logger2.error("Hyderabad, we have a major problem.")
 
-#     logger_provider.shutdown()
-#     return "ok"
+    logger_provider.shutdown()
+    return "ok"
